8b69553b9f72bba52b96bf0132054b5f77c05e25
Daniel Guilak <daniel.guilak@gmail.com>
2013-04-10 19:32:51 +0000

    Added functionality to train from client.

--- ocrn/feature.py
+++ ocrn/feature.py
@@ -1,49 +1,49 @@
-import Image as im
+from PIL import Image as im
 import numpy as np
 import os
 
 class feature:
-	#Return a 10X10 array from a monochrome BMP Image
-	@staticmethod
-	def getImageArray(imagepath):
-		try:
-			image = im.open(imagepath)
-			imagearray=np.asarray(image.crop(image.getbbox()).resize((10,10))).astype(float)
-			return imagearray
-		except IOError:
-			print "File Not Found"
-			return np.zeros((10,10))
-	
-	# Normalizes a 2D array cell.
-	@staticmethod
-	def normalizeArrayCell( array, x, y):
-		for c in range(1,5):
-			for i in range(x-c,x+c+1):
-				for j in range(y-c, y+c+1):
-					if i<=9 and i>=0 and j<=9 and j>=0:
-						if array[i,j] < array[x,y] and array[i,j] < array[x,y]-0.2*c:
-							array[i,j]=array[x,y]-0.2*c
-	
-	# Returns a Normalized 2D Array from an Input Array. 
-	# normalizeArrayCell is called for all non zero cells
-	@staticmethod
-	def getNormalizedArray( imagearray):
-		nonzerocells = np.transpose(imagearray.nonzero())
-		for i in range (0, nonzerocells.shape[0]):
-			feature.normalizeArrayCell(imagearray, nonzerocells[i][0],nonzerocells[i][1])
-		return imagearray
-	
-	# Returns Normalized 2D array from an Image
-	@staticmethod
-	def getNormalizedImageArray( imagepath):
-		return feature.getNormalizedArray(feature.getImageArray(imagepath))
-	
-	# Returns Feature Vector of an Image
-	@staticmethod
-	def getImageFeatureVector( imagepath):
-		array = feature.getNormalizedImageArray(imagepath)
-		vector = np.resize(array,(1,100))
-		return vector[0]
+    #Return a 10X10 array from a monochrome BMP Image
+    @staticmethod
+    def getImageArray(imagepath):
+        try:
+            image = im.open(imagepath)
+            imagearray=np.asarray(image.crop(image.getbbox()).resize((10,10))).astype(float)
+            return imagearray
+        except IOError:
+            print "File Not Found"
+            return np.zeros((10,10))
+    
+    # Normalizes a 2D array cell.
+    @staticmethod
+    def normalizeArrayCell( array, x, y):
+        for c in range(1,5):
+            for i in range(x-c,x+c+1):
+                for j in range(y-c, y+c+1):
+                    if i<=9 and i>=0 and j<=9 and j>=0:
+                        if array[i,j] < array[x,y] and array[i,j] < array[x,y]-0.2*c:
+                            array[i,j]=array[x,y]-0.2*c
+    
+    # Returns a Normalized 2D Array from an Input Array. 
+    # normalizeArrayCell is called for all non zero cells
+    @staticmethod
+    def getNormalizedArray( imagearray):
+        nonzerocells = np.transpose(imagearray.nonzero())
+        for i in range (0, nonzerocells.shape[0]):
+            feature.normalizeArrayCell(imagearray, nonzerocells[i][0],nonzerocells[i][1])
+        return imagearray
+    
+    # Returns Normalized 2D array from an Image
+    @staticmethod
+    def getNormalizedImageArray( imagepath):
+        return feature.getNormalizedArray(feature.getImageArray(imagepath))
+    
+    # Returns Feature Vector of an Image
+    @staticmethod
+    def getImageFeatureVector( imagepath):
+        array = feature.getNormalizedImageArray(imagepath)
+        vector = np.resize(array,(1,100))
+        return vector[0]
 
 
     @staticmethod
@@ -51,9 +51,12 @@ class feature:
         """
         Gets the number of trained images from the imageData file.
         """
-        wcData = os.popen("wc -l ../data/imagedata").read()
+	# Will need to change this to relative path later.
+        wcData = os.popen("wc -l /home/dg/Ocrn/data/inputdata").read()
         # Because wc returns number and filename
-        return wcData.split()[0]
+	wcList = wcData.split()
+	print wcList
+        return int(wcList[0])
         
 
     @staticmethod
@@ -68,13 +71,13 @@ class feature:
         # write that path to imageData with `path:asciiVal`
 
         #This is to not have to do a getTrainingCount call every time.
-        trainCount = getTrainingCount()
-        datafile = open("../data/imagedata", "a")
+        trainCount = feature.getTrainingCount()
+        datafile = open("/home/dg/Ocrn/data/inputdata", "a")
 
         for image in imageData:
-            pathname = "../data/trainingdata/" + trainCount + ".bmp"
+            pathname = "/home/dg/Ocrn/data/trainingdata/" + str(trainCount) + ".bmp"
             image.save(pathname, "BMP")
-            datafile.write(pathname+":"+asciiVal)
-            trainCount++
+            datafile.write(pathname+":"+str(asciiVal)+"\n")
+            trainCount = trainCount + 1 
         
-       datafile.close()
+        datafile.close()
